dacb669d49c76accee13ef69741d8a6ee5490297
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-09-07 00:19:38 -0300

    fix: base styling

--- components/SideNav/SideNav.tsx
+++ components/SideNav/SideNav.tsx
@@ -10,7 +10,7 @@ const SideNav = ({ children }: SideNavProps) => {
 	return (
 		<Container className='max-h-min w-screen p-0 max-w-full'>
 			<Grid container>
-				<Grid container item xs={2} className='h-screen'>
+				<Grid container item xs={2}>
 					<SideNavMenu />
 				</Grid>
 				<Grid item xs={10} className='bg-lightGrey'>
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box'
 import MainView from '@/components/MainView/MainView'
 import { CircularProgress, Typography } from '@mui/material'
-
+import { FileUploader } from '@/components/FileUploader/FileUploader'
 export default function Dashboard() {
 	return (
 		<Box
@@ -13,30 +13,14 @@ export default function Dashboard() {
 		>
 			<Typography
 				marginBottom={10}
-				marginTop='100px'
 				textAlign='center'
-				fontSize='42px'
+				fontSize='2em'
+				maxWidth={800}
 			>
-				Sistema de Apuração dos Registros <br /> Universitários de Extensão
+				Sistema de Apuração dos Registros Universitários de Extensão
 			</Typography>
 
-			<Box
-				display='flex'
-				flexDirection='column'
-				gap={4}
-				width='320px'
-				bgcolor='#006633'
-				m={1}
-				borderRadius='4px'
-				px='40px'
-				py='24px'
-			>
-				<Typography fontSize='40px' fontWeight='bold'></Typography>
-			</Box>
-
-			<Box display='flex' alignItems='center' justifyContent='center' flex={1}>
-				<MainView />
-			</Box>
+			<FileUploader />
 		</Box>
 	)
 }
