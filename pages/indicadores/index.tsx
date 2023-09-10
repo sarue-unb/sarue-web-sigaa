0ea56c230bff19f6859793f8fa5cab439c6fd9a1
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-09-10 18:59:44 -0300

    refactor: rename useIsMobile hook file

--- pages/indicadores/index.tsx
+++ pages/indicadores/index.tsx
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box'
 import { Typography } from '@mui/material'
 import Button from '@mui/material/Button'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight'
-import { useIsMobile } from '@/components/hooks/hooks'
+import { useIsMobile } from '@/components/hooks/useIsMobile'
 
 import {
 	LineChart,
