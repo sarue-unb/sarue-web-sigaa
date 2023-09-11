d8b8addad11c6b6642e68ed5f20a3a5cf36960e3
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-09-11 01:02:36 -0300

    feat: add IndicadoresList

---
+++ components/Indicadores/IndicadoresTcuList.ts
@@ -0,0 +1,15 @@
+export const IndicadoresTcu = {
+	ACOES_INSTITUCIONALIZADAS:
+		'Índice de ações institucionalizadas no SIGAA em relação ao ano anterior',
+	ACOES_MAIS_DE_UMA_UNIDADE_ACADEMICA:
+		'Índice Quantitativo de ações de extensão que possuem mais de uma unidade acadêmica vinculada',
+	ESTUDANTES_EXTENSIONISTAS_POR_PROJETO:
+		'Número de estudantes extensionistas por projeto',
+	PESSOAS_ATENDIDAS:
+		'Total de pessoas diretamente atendidas pelos programas e projetos de extensão',
+	PESSOAS_EXTERNAS_UNB:
+		'Número de pessoas externas à UnB que atuaram como membros da equipe das ações de extensão',
+	ACOES_POR_ODS: 'Quantitativo de ações cadastradas por cada um dos 17 ODS',
+	ACOES_CADASTRADAS_POR_CATEGORIA:
+		'Quantitativo de ações cadastradas por categoria (programas, projetos, cursos e eventos)',
+}
