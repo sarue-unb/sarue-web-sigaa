47e0306e18722e6b3e0b0886643f93575ca10c9c
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-09-15 23:26:09 -0300

    feat: fix prod width

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { Container, Typography } from '@mui/material'
 
 export const Footer = () => {
 	return (
-		<Container className='fixed bottom-0 w-screen p-0 m-0 mr-0 max-w-full bg-darkGreen'>
+		<Container className='occupyWholeScreen fixed bottom-0 w-screen p-0 m-0 mr-0 max-w-full bg-darkGreen'>
 			<Typography variant='body1' sx={{ flexGrow: 1, textAlign: 'center' }}>
 				Universidade de Brasilia
 			</Typography>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,14 @@ import { Footer } from '@/components/Footer/Footer'
 import { Header } from '@/components/Header/Header'
 import { Grid, Box } from '@mui/material'
 import Head from 'next/head'
-import SideNav from '@/components/SideNav/SideNav'
+import MainView from '@/components/MainView/MainView'
 import { useEffect, useState } from 'react'
 import DisclaimerModal from '@/components/Disclaimer/Disclaimer'
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
+			<Head >
 				<title>SARUE</title>
 				<meta
 					name='description'
@@ -20,13 +20,16 @@ export default function App({ Component, pageProps }: AppProps) {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main>
+			<main style={{ maxWidth: '100%' }} className='occupyWholeScreen m-0'>
 				<DisclaimerModal />
-				<Grid className='max-h-screen column'>
+				<Grid
+					sx={{ width: '100%' }}
+					className='occupyWholeScreen max-h-screen column m-0'
+				>
 					<Header />
-					<SideNav>
-						<Component {...pageProps} />
-					</SideNav>
+					<MainView>
+						<Component className='m-0' class='test123' {...pageProps} />
+					</MainView>
 					<Footer />
 				</Grid>
 			</main>
