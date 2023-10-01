f648ddb58671e5e27e977bf3d125b8f8a616da5f
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-10-01 20:12:33 -0300

    refactor: add about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,19 +5,22 @@ const AboutPage = () => {
 		<Box
 			display='flex'
 			flexDirection='column'
-			alignItems='center'
-			justifyContent='center'
+			alignItems='start'
+			justifyContent='start'
 			height='100vh'
+			marginLeft={'2rem'}
+			maxWidth={'30rem'}
 		>
-			<Typography marginTop='100px' textAlign='center' fontSize='42px'>
+			<Typography marginTop='100px' textAlign='center' fontSize='3rem'>
 				Sobre o projeto
 			</Typography>
-			<Box
-				display='flex'
-				alignItems='center'
-				justifyContent='center'
-				flex={1}
-			></Box>
+			<Typography marginTop='100px' textAlign='start' fontSize='1rem'>
+				Este é o Sistema SARUE, desenvolvido por alunos e professores da
+				Universidade de Brasília (UnB) em conjunto com o Decanato de Extensão. O
+				objetivo do sistema é apresentar dados de maneira visual e de fácil
+				compreensão, de diversas atividades realizadas sob a responsabilidade do
+				Decanato de Extensão da Universidade de Brasília.
+			</Typography>
 		</Box>
 	)
 }
