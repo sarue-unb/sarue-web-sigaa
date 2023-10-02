56d7f26e627fa70483a13d7164756b008e8a953c
Carlos Gabriel Soares <carlos.soares@quintoandar.com.br>
2023-10-02 03:01:54 -0300

    fix: icon margin

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 				sx={{ flexGrow: 1, textAlign: 'center' }}
 			>
 				<LeaderboardOutlinedIcon
-					sx={{ marginBottom: '-7px' }}
+					sx={{ marginBottom: '-7px', marginRight: '8px' }}
 					fontSize='large'
 				/>
 				SARUÊ - SIGAA
